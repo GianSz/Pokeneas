8c4728c1690cef6d770981cdd987bbe6a00c406d
Camilo Alvarez <camilo.alvarez@pragma.com.co>
2023-11-05 11:13:56 -0500

    added the images storage in the bucket

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ const data = [
     "name": "Sanchetron",
     "height": 1.73,
     "hability": "Hidratación etílica",
-    "image": "",
+    "image": "https://storage.googleapis.com/pokeneas-storage/createcard.jpg",
     "philosophicalPhrase": "La cerveza, en su efímera espuma y su profundidad de sabor, nos recuerda que la vida es un constante fluir, donde la alegría y la euforia son tan fugaces como un sorbo, pero pueden brindar momentos de deleite y camaradería invaluables."
   },
   {
@@ -12,7 +12,7 @@ const data = [
     "name": "Andrejo",
     "height": 1.69,
     "hability": "Búsqueda de gatas",
-    "image": "",
+    "image": "https://storage.googleapis.com/pokeneas-storage/createcard1.jpg",
     "philosophicalPhrase": "Durante la búsqueda de gatas en la discoteca, encontramos un reflejo de nuestro anhelo de conexión en el laberinto de luces y sonidos, donde la fugaz noche nos recuerda que a menudo perseguimos sombras que solo se desvanecen al amanecer."
   },
   {
@@ -20,7 +20,7 @@ const data = [
     "name": "Felisur",
     "height": 1.70,
     "hability": "Aceleración inimaginable",
-    "image": "",
+    "image": "https://storage.googleapis.com/pokeneas-storage/createcard2.jpg",
     "philosophicalPhrase": "Las carreras de motos son un recordatorio del eterno duelo entre el ser humano y la máquina, donde la velocidad desafía la gravedad y el coraje se convierte en el motor que impulsa nuestros sueños hacia la victoria en el asfalto de la vida."
   },
   {
@@ -28,7 +28,7 @@ const data = [
     "name": "Mapalida",
     "height": 1.75,
     "hability": "Galleta venenosa",
-    "image":"",
+    "image":"https://storage.googleapis.com/pokeneas-storage/createcard3.jpg",
     "philosophicalPhrase": "La vida es como una galleta, a veces es dulce y a veces es amarga, pero siempre es deliciosa."
   },
   {
@@ -36,7 +36,7 @@ const data = [
     "name": "Pinzonio",
     "height": 1.73,
     "hability": "Robo de datos",
-    "image": "",
+    "image": "https://storage.googleapis.com/pokeneas-storage/createcard4.jpg",
     "philosophicalPhrase": "El robo de datos es un acto que nos recuerda que la información es poder, y que la tecnología es un arma de doble filo que puede ser utilizada para el bien o para el mal."
   },
   {
@@ -44,7 +44,7 @@ const data = [
     "name": "Calvocho",
     "height": 1.76,
     "hability": "Gerencia de farras",
-    "image": "",
+    "image": "https://storage.googleapis.com/pokeneas-storage/createcard5.jpg",
     "philosophicalPhrase": "La gerencia de farras es un arte que requiere de una mente fría y calculadora, capaz de tomar decisiones en medio de la euforia y la alegría, pero siempre con la mirada puesta en el objetivo final: la diversión."
   },
   {
@@ -52,7 +52,7 @@ const data = [
     "name": "Danico",
     "height": 1.74,
     "hability": "Conocimiento de la vida",
-    "image": "",
+    "image": "https://storage.googleapis.com/pokeneas-storage/createcard6.jpg",
     "philosophicalPhrase": "La vida es un viaje que nos lleva por caminos inesperados, donde la sabiduría es el combustible que nos permite avanzar hacia el horizonte de la felicidad."
   }
 ]

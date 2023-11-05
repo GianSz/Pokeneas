01925affef5073fc13ce29d84fba215297ae9a50
Camilo Alvarez <camilo.alvarez@pragma.com.co>
2023-11-05 16:07:29 -0500

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
-const DEFAULT_PORT = 3000;
+const DEFAULT_PORT = 8080;
 require('dotenv').config();
 
-app.listen(process.env.PORT || DEFAULT_PORT, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log(`Server listening on port ${process.env.PORT || DEFAULT_PORT}`);
 });
